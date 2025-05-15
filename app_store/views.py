0c28db50b7687ca55da1cef5b03902fd1ae90f4f
serata <serata@serata.ru>
2025-05-15 21:23:54 +0300

    my first commit

---
+++ app_store/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import JsonResponse, HttpResponse
+from .models import DATABASE
+
+
+def product_view_json(request):
+    if request.method == "GET":
+        return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
+                                                         'indent': 4})
+def shop_view(request):
+    if request.method == "GET":
+        with open('app_store/shop.html', encoding="utf-8") as f:
+            data = f.read()  # Читаем HTML файл
+        return HttpResponse(data)  # Отправляем HTML файл как ответ
+
